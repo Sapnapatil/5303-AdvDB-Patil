d1cb403b3a11b046cac29f4a67d293bf5c50806f
Sapna Patil <Sapna.patil1110@gmail.com>
2016-12-11 23:35:07 -0600

    Create api.py

---
+++ mongoDB-Project/api/api.py
@@ -0,0 +1,4 @@
+#Cover sheet with your names on it and information on how to access your api.
+#Name :-Sapna Patil,Tejaswi Prakhya
+#IP ADDRESS to access API of Sapna :- http://67.205.143.154:5000/
+#IP ADDRESS to access API of Prakhya :- http://104.131.64.127:5000/
